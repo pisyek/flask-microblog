7a6444fcbed390b72d30182e44318834bb2662c5
Pisyek Kumar <pisyek@gmail.com>
2020-07-06 15:40:35 +0800

    add user page and remove pyc file

--- app/routes.py
+++ app/routes.py
@@ -49,3 +49,13 @@ def register():
         flash('Congratulations. Your account has been created. Please login.')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'post 1'},
+        {'author': user, 'body': 'post 2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
